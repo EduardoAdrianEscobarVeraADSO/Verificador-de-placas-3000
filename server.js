480e5a6e1fc84c439119ff07bf3c9519d67cba0d
eduardoadrianescobar12@gmail.com <edwin.hernandez@frimac.local>
2024-09-23 13:25:58 -0500

    arreglo package

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { Document, Packer, Paragraph, TextRun } = require('docx');
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
