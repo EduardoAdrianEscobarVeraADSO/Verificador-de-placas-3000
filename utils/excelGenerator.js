715a3b0900e7ad9e362df64aa41e04ed97f0fca1
FrimacProgramAuto <frimaczonafranca@gmail.com>
2024-10-11 08:30:17 -0500

    Feat :rocket: version 1.0

--- utils/excelGenerator.js
+++ utils/excelGenerator.js
@@ -10,50 +10,76 @@ function readJsonFile(filePath) {
 
 function processJsonData(jsonData) {
     const summaryData = [];
-    jsonData.forEach(item => {
-        const baseRow = {
-            Tipo_Documento: item.tipoID,
-            Documento: item.ID,
-            Criterio_busqueda: item.placa_u_documento,
-            Nombre_Propietario: item.nombre_propietario || 'N/A',
-            conductor: item.conductor || "N/A",
-            Correo: item.correo,
-            Comparendos: item.resumen?.comparendos || 'No tiene comparendos ni multas',
-            Multas: item.resumen?.multas || 'No tiene comparendos ni multas',
-            Acuerdos_de_pago: item.resumen?.acuerdos_de_pago || 'No tiene comparendos ni multas',
-            Total: item.resumen?.total || 'No tiene comparendos ni multas'
-        };
 
-        if (item.tabla_multa && item.tabla_multa.length > 0) {
-            item.tabla_multa.forEach(mult => {
+    jsonData.forEach(item => {
+        let acuerdos = "NO";
+
+        if (item.resumen) {
+            if (item.resumen.acuerdos_de_pago > 0) {
+                acuerdos = "SI";
+            } else {
+                acuerdos = "NO";
+            }
+        }
+
+        
+
+            const baseRow = {
+                Tipo_Documento: item.tipoID,
+                Documento: item.ID,
+                conductor: item.conductor || "N/A",
+                Nombre_Propietario: item.nombre_propietario || 'N/A',
+                Correo: item.correo,
+                Total: item.resumen?.total || 'No tiene comparendos ni multas',
+            };
+
+            if (item.tabla_multa && item.tabla_multa.length > 0) {
+                item.tabla_multa.forEach(mult => {
+                    const descriptions = require('../description.json');
+                    let tipoMulta = mult.tipo;
+                    let fechaMult = tipoMulta.match(/\b\d{2}\/\d{2}\/\d{4}\b/);
+                    let typeMult = tipoMulta.match(/multa|comparendo/i);
+                    let descripcion = "Descripción no disponible";
+                    for (const key in descriptions) {
+                        if (mult.infraccion.includes(key)) {
+                            descripcion = descriptions[key];
+                            break;
+                        }
+                    }
+                        summaryData.push({
+                        ...baseRow,
+                        Fecha_multa: fechaMult[0],
+                        Acuerdos_de_pago: acuerdos,
+                        Valor: mult.valor,
+                        tipo: typeMult[0],
+                        Placa: mult.placa,
+                        Infraccion: mult.infraccion,
+                        Ciudad_de_la_infraccion: mult.secretaria,
+                        Organismo_de_transito: mult.secretaria,
+                        Estado: mult.estado,
+                        Descripcion: descripcion,
+                    });
+                });
+            } else {
+
                 summaryData.push({
                     ...baseRow,
-                    Tipo: mult.tipo,
-                    Notificacion: mult.notificacion,
-                    Secretaria: mult.secretaria,
-                    Placa: mult.placa,
-                    Infraccion: mult.infraccion,
-                    Estado: mult.estado,
-                    Valor: mult.valor,
-                    Valor_a_pagar: mult.valor_a_pagar
+                    Tipo: 'N/A',
+                    Notificacion: 'N/A',
+                    Infraccion: 'N/A',
+                    Placa: 'N/A',
+                    Valor: 'N/A',
+                    Ciudad_de_la_infraccion: 'N/A',
+                    Organismo_de_transito: 'N/A',
+                    Estado: 'N/A',
+                    Valor_a_pagar: 'N/A'
                 });
-            });
-        } else {
-            summaryData.push({ 
-                ...baseRow, 
-                Tipo: 'N/A', 
-                Notificacion: 'N/A', 
-                Secretaria: 'N/A', 
-                Infraccion: 'N/A', 
-                Estado: 'N/A', 
-                Valor: 'N/A', 
-                Valor_a_pagar: 'N/A' 
-            });
-        }
-    });
+            }
+        });
 
     return summaryData;
 }
+
 
 function generateExcel(data, fileName) {
     const wb = XLSX.utils.book_new();
@@ -79,7 +105,7 @@ function generateExcel(data, fileName) {
     XLSX.utils.book_append_sheet(wb, ws, 'Resultados');
     const filePath = path.join(__dirname, fileName);
     XLSX.writeFile(wb, filePath);
-    
+
     return filePath;
 }
 
