8baa631220093217fa3354c1b0f1d678c2169498
FrimacProgramAuto <frimaczonafranca@gmail.com>
2024-10-17 15:05:12 -0500

    avance

--- public/main.js
+++ public/main.js
@@ -1,153 +1,17 @@
-function validarFor(event) {
-    let input = document.getElementById("placa-input").value.trim();
-
-    if (input === "") {
-        alert("Los campos no pueden quedar vacíos");
-        event.preventDefault();
-        return false;
-    }
-
-    return true;
-}
-function mostrarNotificacion() {
-    const notificationContainer = document.getElementById('notification-container');
-
-    const notification = document.createElement('div');
-    notification.className = 'notification';
-    notification.innerHTML = `
+function validarFor(event){let input=document.getElementById("placa-input").value.trim();if(input===""){alert("Los campos no pueden quedar vacíos");event.preventDefault();return!1}
+return!0}
+function mostrarNotificacion(){const notificationContainer=document.getElementById('notification-container');const notification=document.createElement('div');notification.className='notification';notification.innerHTML=`
         Puedes descargar tanto el excel como las cartas del último proceso de consulta!
-    `;
-
-    notificationContainer.appendChild(notification);
-}
-
-function cerrarNotificacion(btn) {
-    const notification = btn.parentElement;
-    notification.remove();
-}
-
-
-async function consultarPlacas(event) {
-    if (!validarFor(event)) {
-        return;
-    }
-
-    // Deshabilitar el botón de "Consultar"
-    const botonConsultar = document.querySelector('form button[type="submit"]');
-    botonConsultar.disabled = true;
-    botonConsultar.innerText = 'Consultando...'; // Cambiar el texto del botón mientras carga
-
-    // Mostrar el loader
-    document.getElementById('loader').style.display = 'block';
-
-    const placaInput = document.getElementById('placa-input').value;
-    const response = await fetch('/consultar', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: `placa=${encodeURIComponent(placaInput)}`
-    });
-
-    const result = await response.json();
-    console.log(result);
-    
-    const sonido = document.getElementById('sonido-notificacion');
-
-    try {
-        await sonido.play(); // Esperar a que se reproduzca el sonido
-    } catch (error) {
-        console.error('Error al reproducir el sonido:', error);
-    }
-
-    // Ocultar el loader después de que se complete la consulta
-    document.getElementById('loader').style.display = 'none';
-
-    // Habilitar nuevamente el botón de "Consultar"
-    botonConsultar.disabled = false;
-    botonConsultar.innerText = 'Consultar'; // Restaurar el texto original del botón
-
-    // Mostrar el alert después de reproducir el sonido
-    alert(result.message);
-
-    // Mostrar los botones de descarga
-    document.getElementById('download-btn').style.display = 'block';
-    document.getElementById('download-btnWord').style.display = 'block';
-}
-
-
-async function descargarCartas() {
-    const response = await fetch('/descargar-cartas', {
-        method: 'GET',
-    });
-
-    if (response.ok) {
-        const blob = await response.blob();
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'cartas.zip'; // Nombre del archivo descargado
-        document.body.appendChild(a);
-        a.click();
-        a.remove();
-        window.URL.revokeObjectURL(url);
-    } else {
-        console.error('Error al descargar las cartas');
-    }
-}
-
-function descargarExcel() {
-    window.location.href = '/download-excel';
-}
-
-async function enviarCorreo() {
-    const response = await fetch('/enviar-correos', {
-        method: 'POST',
-    });
-
-    if (response.ok) {
-        const blob = await response.blob(); // Convertir la respuesta en un blob
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'cartas.zip'; // Nombre del archivo descargado
-        document.body.appendChild(a);
-        a.click();
-        a.remove();
-        window.URL.revokeObjectURL(url);
-    } else {
-        console.error('Error al enviar correos:', response.statusText);
-        alert('Error al enviar correos: ' + response.statusText); // Muestra el error en un mensaje
-    }
-}
-
-// Función para cargar el JSON y generar la tabla
-async function cargarTabla() {
-    try {
-        const response = await fetch('/api/resultados'); // Ruta del JSON o API
-        if (!response.ok) {
-            throw new Error('Error al cargar los datos.');
-        }
-        
-        const data = await response.json(); // Cargar los datos del JSON
-        
-        // Filtrar los datos para incluir solo los que tienen comparendos o multas
-        const datosFiltrados = data.filter(item => 
-            item.resumen && 
-            (item.resumen.comparendos > "0" || item.resumen.multas > "0")
-        );
-
-        const tablaContenedor = document.getElementById('tabla-contenedor');
-        tablaContenedor.innerHTML = ''; // Limpiar contenido anterior
-
-        if (datosFiltrados.length > 0) {
-            const tabla = document.createElement('table');
-            tabla.className = 'tabla-resultados'; // Estilos de la tabla
-
-            // Crear el encabezado de la tabla
-            const thead = document.createElement('thead');
-            const encabezado = document.createElement('tr');
-            encabezado.innerHTML = `
+    `;notificationContainer.appendChild(notification)}
+function cerrarNotificacion(btn){const notification=btn.parentElement;notification.remove()}
+async function consultarPlacas(event){if(!validarFor(event)){return}
+const botonConsultar=document.querySelector('form button[type="submit"]');botonConsultar.disabled=!0;botonConsultar.innerText='Consultando...';document.getElementById('loader').style.display='block';const placaInput=document.getElementById('placa-input').value;const response=await fetch('/consultar',{method:'POST',headers:{'Content-Type':'application/x-www-form-urlencoded',},body:`placa=${encodeURIComponent(placaInput)}`});const result=await response.json();console.log(result);const sonido=document.getElementById('sonido-notificacion');try{await sonido.play()}catch(error){console.error('Error al reproducir el sonido:',error)}
+document.getElementById('loader').style.display='none';botonConsultar.disabled=!1;botonConsultar.innerText='Consultar';alert(result.message);document.getElementById('download-btn').style.display='block';document.getElementById('download-btnWord').style.display='block'}
+async function descargarCartas(){const response=await fetch('/descargar-cartas',{method:'GET',});if(response.ok){const blob=await response.blob();const url=window.URL.createObjectURL(blob);const a=document.createElement('a');a.href=url;a.download='cartas.zip';document.body.appendChild(a);a.click();a.remove();window.URL.revokeObjectURL(url)}else{console.error('Error al descargar las cartas')}}
+function descargarExcel(){window.location.href='/download-excel'}
+async function enviarCorreo(){const response=await fetch('/enviar-correos',{method:'POST',});if(response.ok){const blob=await response.blob();const url=window.URL.createObjectURL(blob);const a=document.createElement('a');a.href=url;a.download='cartas.zip';document.body.appendChild(a);a.click();a.remove();window.URL.revokeObjectURL(url)}else{console.error('Error al enviar correos:',response.statusText);alert('Error al enviar correos: '+response.statusText)}}
+async function cargarTabla(){try{const response=await fetch('/api/resultados');if(!response.ok){throw new Error('Error al cargar los datos.')}
+const data=await response.json();const datosFiltrados=data.filter(item=>item.resumen&&(item.resumen.comparendos>"0"||item.resumen.multas>"0"));const tablaContenedor=document.getElementById('tabla-contenedor');tablaContenedor.innerHTML='';if(datosFiltrados.length>0){const tabla=document.createElement('table');tabla.className='tabla-resultados';const thead=document.createElement('thead');const encabezado=document.createElement('tr');encabezado.innerHTML=`
                 <th>Placa/Documento</th>
                 <th>Propietario</th>
                 <th>Tipo de Infracción</th>
@@ -155,18 +19,7 @@ async function cargarTabla() {
                 <th>Infracción</th>
                 <th>Estado</th>
                 <th>Valor a Pagar</th>
-            `;
-            thead.appendChild(encabezado);
-            tabla.appendChild(thead);
-
-            // Crear el cuerpo de la tabla
-            const tbody = document.createElement('tbody');
-            datosFiltrados.forEach(item => {
-                if (item.tabla_multa) { // Verificar si existe tabla_multa
-                    item.tabla_multa.forEach(multa => {
-                        if (multa.tipo.includes("Comparendo") || multa.tipo.includes("Multa")) {
-                            const fila = document.createElement('tr');
-                            fila.innerHTML = `
+            `;thead.appendChild(encabezado);tabla.appendChild(thead);const tbody=document.createElement('tbody');datosFiltrados.forEach(item=>{if(item.tabla_multa){item.tabla_multa.forEach(multa=>{if(multa.tipo.includes("Comparendo")||multa.tipo.includes("Multa")){const fila=document.createElement('tr');fila.innerHTML=`
                                 <td>${item.placa_u_documento}</td>
                                 <td>${item.nombre_propietario}</td>
                                 <td>${multa.tipo.includes("Comparendo") ? 'Comparendo' : 'Multa'}</td>
@@ -174,28 +27,6 @@ async function cargarTabla() {
                                 <td>${multa.infraccion}</td>
                                 <td>${multa.estado}</td>
                                 <td>${multa.valor_a_pagar.split('Detalle Pago')[0]}</td>
-                            `;
-                            tbody.appendChild(fila);
-                        }
-                    });
-                }
-            });
-            tabla.appendChild(tbody);
-            tablaContenedor.appendChild(tabla);
-        } else {
-            tablaContenedor.innerHTML = '<p>No hay comparendos o multas pendientes.</p>';
-        }
-    } catch (error) {
-        console.error('Error al cargar la tabla:', error);
-    }
-}
-
-function buscarTodos() {
-    window.location.href = '/buscarTodos';
-}
-
-// Llamar a la función para cargar la tabla al cargar la página
-window.onload = () => {
-    cargarTabla();
-    mostrarNotificacion();
-};
+                            `;tbody.appendChild(fila)}})}});tabla.appendChild(tbody);tablaContenedor.appendChild(tabla)}else{tablaContenedor.innerHTML='<p>No hay comparendos o multas pendientes.</p>'}}catch(error){console.error('Error al cargar la tabla:',error)}}
+function buscarTodos(){window.location.href='/buscarTodos'}
+window.onload=()=>{cargarTabla();mostrarNotificacion()}
--- utils/allFindFunctions.js
+++ utils/allFindFunctions.js
@@ -1,9 +1,12 @@
-
 const { authApi } = require('./findFunctions');
 
+// Lista de operaciones
+const operaciones = [
+    "COMER", "ALIM", "REFRI", "POLPIE", "SUBPRO", "CANAS", "COMTRA", "CARSEC", "POLLI", "INSUM", "DSECS", "HARI", "HFERT", "MATPRI"
+];
+
 async function allUsers() {
-    const url = "https://tcfrimac.simplexity.com.co/OData/api/Tc4ViewUcrTercero?";
-    const token = await authApi();
+    const token = await authApi(); // Autenticación obteniendo el token
     const options = {
         method: 'GET',
         headers: {
@@ -12,47 +15,50 @@ async function allUsers() {
         }
     };
 
-    try {
-        const response = await fetch(url, options);
+    let todosCodigosUnicos = []; // Acumulador para todos los códigos de usuarios
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
+    // Bucle que recorre cada operación
+    for (const operacion of operaciones) {
+        // Construir la URL con la operación interpolada
+        const url = `https://tcfrimac.simplexity.com.co/OData/api/Tc4ViewUcrTercero?%24filter=(UtyCode%20eq%20%27CON%27)%20and%20(State%20eq%20%271%27)%20and%20(UcrBusinessOperationTypeBopCode%20eq%20%27${operacion}%27)`;
+
+        try {
+            const response = await fetch(url, options);
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status} para la operación ${operacion}`);
+            }
+
+            const data = await response.json();
+
+            if (data.value.length === 0) {
+                console.log(`No se encontraron usuarios para la operación ${operacion}.`);
+                continue; // Saltar a la siguiente operación
+            }
+
+            // Obtener todos los Ucr_Code de los usuarios
+            const codigosUsuarios = data.value.map(usuario => usuario.Ucr_Code);
+
+            // Eliminar duplicados utilizando un Set
+            const codigosUnicos = [...new Set(codigosUsuarios)];
+
+            // Acumular los códigos únicos en el array principal
+            todosCodigosUnicos = [...new Set([...todosCodigosUnicos, ...codigosUnicos])];
+
+        } catch (error) {
+            console.error(`Hubo un error en la solicitud para la operación ${operacion}:`, error);
         }
-
-        const data = await response.json();
-
-        if (data.value.length === 0) {
-            console.log('No se encontraron usuarios.');
-            return [];
-        }
-
-        // Filtrar los usuarios activos 
-        const usuariosActivos = data.value.filter(usuario => usuario.State !== 2);
-
-        if (usuariosActivos.length === 0) {
-            console.log('No se encontraron usuarios activos.');
-            return [];
-        }
-
-        // Obtener todos los Ucr_Code de los usuarios activos
-        const codigosActivos = usuariosActivos.map(usuario => usuario.Ucr_Code);
-
-        // Eliminar duplicados utilizando un Set
-        const codigosUnicos = [...new Set(codigosActivos)];
-
-        console.log('Códigos únicos de usuarios activos:', codigosUnicos);
-        return codigosUnicos; // Retorna la lista de códigos únicos
-
-    } catch (error) {
-        console.error('Hubo un error en la solicitud:', error);
-        return [];
     }
+
+    // Retornar todos los códigos únicos de usuarios
+    return todosCodigosUnicos;
 }
 
+module.exports = { allUsers };
 
 
 async function allPlates() {
-    const url = "https://tcfrimac.simplexity.com.co/OData/api/Tc4ViewVehicle?";
+    const url = "https://tcfrimac.simplexity.com.co/OData/api/Tc4ViewVehicle?$filter=(SocId%20eq%2053)%20and%20((VcnType%20eq%20%27TRUCK%27)%20%20%20%20%20%20%20%20%20or%20(VcnType%20eq%20%27HEAD%27)%20or%20(VcnType%20eq%20%27SET%27))%20and%20contains(Plate,%27*%27)%20or%20contains(DriverName,%27*%27)%20or%20contains(CarrierName,%27*%27)%20or%20contains(Trailer,%27*%27)%20or%20contains(TraPlate,%27*%27)%20or%20contains(OwnerName,%27*%27)";
     const token = await authApi();
     const options = {
         method: 'GET',
@@ -78,7 +84,7 @@ async function allPlates() {
 
         // Filtrar las placas activas y de tipo TRUCK
         const placasActiva = data.value.filter(placas => 
-            placas.StaName !== "Inactivo" && placas.VcnType === "TRUCK" || placas.VcnType === "TRUCK" 
+            placas.StaName !== "Inactivo"  
         );
 
         if (placasActiva.length === 0) {
@@ -86,7 +92,7 @@ async function allPlates() {
             return [];
         }
         
-        const placasActivas = placasActiva.map(placas => placas.Code);
+        const placasActivas = data.value.map(placas => placas.Code);
 
         return placasActivas;
 
